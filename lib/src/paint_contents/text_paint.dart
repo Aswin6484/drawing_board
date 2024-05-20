53c6aca329d599c835d179246b1a40710e2c933d
SafdherTrenser <167956020+SafdherTrenser@users.noreply.github.com>
2024-05-20 23:13:08 +0530

    Revert "Color Change"

--- lib/src/paint_contents/text_paint.dart
+++ lib/src/paint_contents/text_paint.dart
@@ -43,7 +43,7 @@ class TextPaint extends PaintContent {
   Offset position = Offset.zero;
   String text = '';
   String uiText = '';
-  Color textColor = Colors.white;
+  Color textColor = Colors.black;
   int fontSize = 30;
   bool isPipe = true;
 
